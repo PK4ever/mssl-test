755fbe46a87cafa06895601175caeb6a5b830268
pk4ever <paulkobe.pk@gmail.com>
2017-06-14 00:37:16 -0400

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,7 +37,7 @@ public class Main {
 //        String databaseUrl = "jdbc:postgresql://localhost:5432/mssl?characterEncoding=UTF-8&useSSL=false";
 
 //        String databaseUrl = "jdbc:mysql://manliussumm_8345";
-        String databaseUrl = "jdbc:mysql://manliussumm_8345:31318/manliussumm_8345";
+        String databaseUrl = "jdbc:mysql://manliussumm_8345:31318";
 
 
 
